5e333e68f9a686c033a2f14d132c7d95d36b2d2e
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-08-08 20:13:08 +0100

    image collision detection

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'rainbow-gradient': 'linear-gradient(to right, #FF0000 0%, #55FF00 25%, #00FFEE 60%, #E500FF 100%)',
+			},
+		},
 	},
 	plugins: [],
 }
