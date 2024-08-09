da3d1763c05f53e236c85d39eee78ceda93f5622
Oleksii Nazarenko <alexeybread81@gmail.com>
2024-08-09 18:28:26 +0100

    add Problem section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			boxShadow: {
+				'lg': '0px 4px 20px rgba(0, 0, 0, 0.25)',
+			},
 		},
 	},
 	plugins: [],
